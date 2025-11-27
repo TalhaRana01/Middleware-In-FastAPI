198eca592fb2be7ea7bd4ce5d09bf3901e2cb39d
Talha Rana <mtalharana093@gmail.com>
2025-11-28 03:12:45 +0500

    middleware execulation order in multiple middleware

--- app/main.py
+++ app/main.py
@@ -11,6 +11,20 @@ async def my_first_middleware(request: Request, call_next):
   
   response = await call_next(request)
   
+  print("1 Middleware: After processing the request, Before returning  the response")
+  print(f"Response status code :{response.status_code}")
+  
+  
+  return response
+
+
+@app.middleware("http")
+async def my_second_middleware(request: Request, call_next):
+  print("2 Middleware: Before processing the request")
+  print(f"Request:{request.method} {request.url}")
+  
+  response = await call_next(request)
+  
   print("Middleware: After processing the request, Before returning  the response")
   print(f"Response status code :{response.status_code}")
   
