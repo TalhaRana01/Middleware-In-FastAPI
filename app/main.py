d6420e65bf22d5229a9c3f243e48a8aed19e04d6
Talha Rana <mtalharana093@gmail.com>
2025-11-28 03:51:25 +0500

    built-in middleware in fastapi

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 # from app.middlewares import my_first_middleware, my_second_middleware
 from app.middlewares import user_only_middleware, product_only_middleware
 
 
 app = FastAPI()
 
-app.middleware("http")(product_only_middleware)
-app.middleware("http")(user_only_middleware)
+# Custom Middleware
+# app.middleware("http")(product_only_middleware)
+# app.middleware("http")(user_only_middleware)
 
+# Built-In Middleware
+app.middleware("http")(HTTPSRedirectMiddleware)
 
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
 
 @app.get("/users")
 async def get_users():
