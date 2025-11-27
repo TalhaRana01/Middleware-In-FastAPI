e12d7010c78e065ab968166eae4fe27a3989834c
Talha Rana <mtalharana093@gmail.com>
2025-11-28 03:07:31 +0500

    understanding of middlewares

--- app/main.py
+++ app/main.py
@@ -1,4 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 app = FastAPI()
+
+# Creating Middleware
+@app.middleware("http")
+async def my_first_middleware(request: Request, call_next):
+  print("Middleware: Before processing the request")
+  print(f"Request:{request.method} {request.url}")
+  
+  response = await call_next(request)
+  
+  print("Middleware: After processing the request, Before returning  the response")
+  print(f"Response status code :{response.status_code}")
+  
+  
+  return response
+
+@app.get("/users")
+async def get_users():
+  print("Endpoint :  Inside get_users endpoint")
+  return {"data": "All users data"}
+  
