9f995d49a98505f4c08b99f284185a36a38e4ea3
Talha Rana <mtalharana093@gmail.com>
2025-11-28 03:39:57 +0500

    user and product execution middlewares

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
-from app.middlewares import my_first_middleware, my_second_middleware
+# from app.middlewares import my_first_middleware, my_second_middleware
+from app.middlewares import user_only_middleware, product_only_middleware
 
 
 app = FastAPI()
 
-app.middleware("http")(my_first_middleware)
-app.middleware("http")(my_second_middleware)
+app.middleware("http")(product_only_middleware)
+app.middleware("http")(user_only_middleware)
 
 
 
 @app.get("/users")
 async def get_users():
   print("Endpoint :  Inside get_users endpoint")
-  return {"data": "All users data"}
+  return {"data": "All Users data"}
+
+@app.get("/products")
+async def get_users():
+  print("Endpoint :  Inside get_users endpoint")
+  return {"data": "All Products data"}
   
--- app/middlewares.py
+++ app/middlewares.py
@@ -4,28 +4,60 @@ from fastapi import Request
 # Creating Middleware
 
 # First Middleware
-async def my_first_middleware(request: Request, call_next):
-  print("2 Middleware: Before processing the request")
-  print(f"Request:{request.method} {request.url}")
+# async def my_first_middleware(request: Request, call_next):
+#   print("2 Middleware: Before processing the request")
+#   print(f"Request:{request.method} {request.url}")
   
-  response = await call_next(request)
+#   response = await call_next(request)
   
-  print("Middleware: After processing the request, Before returning  the response")
-  print(f"Response status code :{response.status_code}")
+#   print("Middleware: After processing the request, Before returning  the response")
+#   print(f"Response status code :{response.status_code}")
   
   
-  return response
+#   return response
+
+
+# Middleware for User Route
+async def user_only_middleware(request: Request, call_next):
+  if request.url.path.startswith("/users"):
+    print("User Middleware: Before processing the request")
+    response = await call_next(request)
+    print("User Middleware: After processing the request, Before returning  the response")
+    print(f"Response status code :{response.status_code}")
+    return response
+  else:
+    print(f"User Middleware: Skipping middleware for {request.url.path}")
+    response = await call_next(request)
+    return response
+    
+    
+  
+  
+  
+  
 
 
 # Second Middleware
-async def my_second_middleware(request: Request, call_next):
-  print("2 Middleware: Before processing the request")
-  print(f"Request:{request.method} {request.url}")
+# async def my_second_middleware(request: Request, call_next):
+#   print("2 Middleware: Before processing the request")
+#   print(f"Request:{request.method} {request.url}")
   
-  response = await call_next(request)
+#   response = await call_next(request)
   
-  print("Middleware: After processing the request, Before returning  the response")
-  print(f"Response status code :{response.status_code}")
+#   print("Middleware: After processing the request, Before returning  the response")
+#   print(f"Response status code :{response.status_code}")
   
   
-  return response
+#   return response
+# Second Middleware
+async def product_only_middleware(request: Request, call_next):
+    if request.url.path.startswith("/products"):
+      print("Product Middleware: Before processing the request")
+      response = await call_next(request)
+      print(" Product Middleware: After processing the request, Before returning  the response")
+      print(f"Response status code :{response.status_code}")
+      return response
+    else:
+      print(f"Prdouct Middleware: Skipping middleware for {request.url.path}")
+      response = await call_next(request)
+      return response
